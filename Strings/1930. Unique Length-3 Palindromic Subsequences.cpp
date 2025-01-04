6a71b6adaa298123e42bd94a32e74f58950e8503
TharunKumarrA <tharunkumarra@gmail.com>
2025-01-05 00:23:58 +0530

    Problem Difficulty Level : Medium

---
+++ Strings/1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -0,0 +1,49 @@
+
+    /*
+    Time complexity : O(n)
+    Space complexity : O(n)
+    */
+
+
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_set<char> letters;
+        for (char c : s) {
+            letters.insert(c);
+        }
+        
+        int ans = 0;
+        for (char letter : letters) {
+            int i = -1;
+            int j = 0;
+            
+            for (int k = 0; k < s.size(); k++) {
+                if (s[k] == letter) {
+                    if (i == -1) {
+                        i = k;
+                    }
+                    
+                    j = k;
+                }
+            }
+            
+            unordered_set<char> between;
+            for (int k = i + 1; k < j; k++) {
+                between.insert(s[k]);
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
+    }
+};
+
+
+    /*
+    Author : TharunKumarrA
+    Question Link : https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+    */
+
+    

48632ffe6df2d852bb5054f4b6be652a4d59a735
TharunKumarrA <tharunkumarra@gmail.com>
2025-01-20 13:08:55 +0530

    Problem Difficulty Level : Medium

---
+++ Stack/735. Asteroid Collision.cpp
@@ -0,0 +1,49 @@
+
+    /*
+    Time complexity : Not Defined
+    Space complexity : O(n)
+    */
+
+
+#include <stack>
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+
+        for (int ast : asteroids) {
+            if (ast > 0) {
+                s.push(ast);
+            }
+            else {
+                while (!s.empty() && s.top() > 0 && s.top() < -ast) {
+                    s.pop();
+                }
+                if (s.empty() || s.top() < 0) {
+                    s.push(ast);
+                }
+                if (!s.empty() && s.top() == -ast) {
+                    s.pop();
+                }
+            }
+        }
+        vector<int> res(s.size());
+        int i = s.size() - 1;
+
+        while (!s.empty()) {
+            res[i--] = s.top();
+            s.pop();
+        }
+
+        return res; 
+    }
+};
+
+
+    /*
+    Author : TharunKumarrA
+    Question Link : https://leetcode.com/problems/asteroid-collision/
+    */
+
+    
